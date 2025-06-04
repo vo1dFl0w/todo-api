2d8d25dabb656cdc336e6fef56a6fc249598d2cd
voidflow <sparkles.exist@yandex.ru>
2025-06-04 12:21:11 +0300

    server configuration

---
+++ internal/app/apiserver/config.go
@@ -0,0 +1,28 @@
+package apiserver
+
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env 	 string `yaml:"env" env-default:"local" env-required:"true"`
+	HTTPAddr string `yaml:"httpaddr" env-default:"localhost:8080" env-required:"true"`
+}
+
+func NewConfig() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("config path is not set")
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("config is not exist: %s", err)
+	}
+
+	return &cfg
+}
